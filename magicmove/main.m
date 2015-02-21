f47de65b1511611795c0728059fe315bce8ea9a0
Stanislav Musil <logicry@gmail.com>
2015-02-21 22:21:16 +0100

    Initial Commit

---
+++ magicmove/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  magicmove
+//
+//  Created by Stanislav Musil on 2/21/15.
+//  Copyright (c) 2015 scriptylab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
